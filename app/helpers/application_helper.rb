540fa753340a44f5b0b21d067e9a2bc703f27196
Andrew Frank <andrewpaulfrank@gmail.com>
2012-11-28 14:01:12 -0300

    highlight column currently sorted

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,18 @@
 module ApplicationHelper
     def sortable(column, title=nil)
         title ||= column.titleize
+        
         if column == sort_column && sort_direction == "asc"
             direction = "desc"
         else direction = "asc"
         end
-        link_to title, :sort => column, :direction => direction
+
+        if column == sort_column
+            css_class = "hilite"
+        else css_class = nil
+        end
+
+        link_to title, {:sort => column, :direction => direction}, 
+        {:class => css_class}
     end    
 end
